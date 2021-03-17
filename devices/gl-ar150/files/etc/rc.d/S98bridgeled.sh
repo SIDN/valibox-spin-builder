790b70e27a32c37f27d2692d2c222a3a90ce4d69
Elmer Lastdrager <elmer.lastdrager@sidn.nl>
2021-03-17 11:36:04 +0100

    Update bridge mode startup logic

--- devices/gl-ar150/files/etc/rc.d/S98bridgeled.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-CHECK_FILE="/etc/bridgemode.on"
-
-if [ -f "$CHECK_FILE" ]; then
-    echo 1 > /sys/class/leds/gl-ar150\:orange\:wlan/brightness
-else
-    echo 0 > /sys/class/leds/gl-ar150\:orange\:wlan/brightness
-fi
