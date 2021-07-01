a1931e38c0cfbcf7d2cf1c087d5ba4060a476951
Elmer Lastdrager <elmer.lastdrager@sidn.nl>
2021-07-01 10:41:37 +0200

    Reverse button state

--- devices/gl-ar150/files/sbin/bridge.sh
+++ devices/gl-ar150/files/sbin/bridge.sh
@@ -44,7 +44,7 @@ elif [ "${COMMAND}" == "off" ]; then
     exit 0
 else
     # Check status, return 0 when in bridge mode, 1 otherwise.
-    NOTHING=$(cat /sys/kernel/debug/gpio | grep BTN_8 | grep lo)
+    NOTHING=$(cat /sys/kernel/debug/gpio | grep BTN_8 | grep hi)
     STATUS=$?
     # STATUS is 0 (default) or 1 (bridge mode or error)
     exit $STATUS
